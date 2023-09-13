fe2b3c2074eecef1658d2370d032f45d9b621982
NyanLight <128150876+NyanLight@users.noreply.github.com>
2023-09-13 16:43:11 +0400

    create operate function

--- index.js
+++ index.js
@@ -1,20 +1,38 @@
-let firstNumber; 
+let firstNumber;
 let secondNumber;
-let operator; 
+let operator;
 
-function add (a, b) {
-    result = a + b; 
-};
+function add(firstNumber, secondNumber) {
+  result = firstNumber + secondNumber;
+}
 
-function subtract (a, b) {
-    result = a - b;
-};
+function subtract(firstNumber, secondNumber) {
+  result = firstNumber - secondNumber;
+}
 
-function multiply (a, b) {
-    result = a * b;
-};
+function multiply(firstNumber, secondNumber) {
+  result = firstNumber * secondNumber;
+}
 
-function divide (a, b) {
-    result = a / b; 
-};
+function divide(firstNumber, secondNumber) {
+  result = firstNumber / secondNumber;
+}
 
+function operate(firstNumber, secondNumber, operator) {
+  switch (operator) {
+    case "+":
+      add(firstNumber, secondNumber);
+      break;
+    case "-":
+        subtract(firstNumber, secondNumber);
+        break;
+    case "*": 
+        multiply(firstNumber, secondNumber);
+        break;
+    case "/":
+        divide(firstNumber, secondNumber);
+        break;
+    default:
+        alert('Wrong expression'); 
+  }
+}
