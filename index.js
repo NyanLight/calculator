7fa22866a871bdd695cbc74912afcf41ac50897c
NyanLight <128150876+NyanLight@users.noreply.github.com>
2023-09-13 23:20:42 +0400

    add query selector for each button

--- index.js
+++ index.js
@@ -2,6 +2,27 @@ let firstNumber;
 let secondNumber;
 let operator;
 
+const cancelButton = document.querySelector('#cButton');
+const cancelEntryButton = document.querySelector('#ceButton');
+const nullButton = document.querySelector('#nullButton');
+const oneButton = document.querySelector('#oneButton');
+const twoButton = document.querySelector('#twoButton');
+const threeButton = document.querySelector('#threeButton');
+const fourButton = document.querySelector('#fourButton');
+const fiveButton = document.querySelector('#fiveButton');
+const sixButton = document.querySelector('#sixButton');
+const sevenButton = document.querySelector('#sevenButton');
+const eightButton = document.querySelector('#eightButton');
+const nineButton = document.querySelector('#nineButton');
+const equalButton = document.querySelector('#equalButton');
+const plusButton = document.querySelector('#plusButton');
+const minusButton = document.querySelector('#minusButton');
+const multiplyButton = document.querySelector('#multiplyButton');
+const divideButton = document.querySelector('#divideButton');
+
+
+
+
 function add(firstNumber, secondNumber) {
   result = firstNumber + secondNumber;
 }
